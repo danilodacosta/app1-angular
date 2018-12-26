78005b4ca0cbc22b7e1a97d87599bb11a0bf2be6
Danilo Pereira <danilo-gc@hotmail.com>
2018-12-26 18:30:06 -0300

    Componente tentativas, frases e limpando campos dinamicamentes.

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -11,9 +11,58 @@ export class PainelComponent implements OnInit {
 
   public frases: Frase[] = FRASES;
   public instrucao = 'Traduza a frase:';
-  constructor() {}
+  public resposta = '';
 
-  ngOnInit() {
+  public rodada = 0;
+  public rodadaFrase: Frase;
+
+  public progresso = 0;
+
+  public tentativas = 3;
+
+  constructor() {
+    this.atualizaRodada();
   }
 
+  ngOnInit() {}
+
+  public atualizaResposta(resposta: Event): void {
+    this.resposta = (<HTMLInputElement>resposta.target).value;
+  }
+
+  public verificaResposta(): void {
+    if (this.rodadaFrase.frasePtBr === this.resposta) {
+
+        alert('Resposta correta');
+
+        // trocar a pergunta da rodada
+        this.rodada++;
+
+        // progresso
+        this.progresso = this.progresso + (100 / this.frases.length);
+        console.log(this.progresso);
+
+        // atualiza o objeto da frase
+       this.atualizaRodada();
+
+      } else {
+       // diminuir a variável tentativas
+       this.tentativas --;
+
+       if (this.tentativas === -1) {
+          alert('Usuario vocÊ perdeu todas as tentativas.');
+       }
+
+
+    }
+  }
+
+  public atualizaRodada(): void {
+
+    // define a frase da rodada com base em alguma logica
+    this.rodadaFrase = this.frases[this.rodada];
+
+    // limpar a resposta
+    this.resposta = '';
+  }
 }
--- src/app/tentativas/tentativas.component.ts
+++ src/app/tentativas/tentativas.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import {Coracao} from '../shared/coracao.model';
 
 @Component({
   selector: 'app-tentativas',
@@ -7,8 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TentativasComponent implements OnInit {
 
-  public coracaoVazio = '/assets/coracao_vazio.png';
-  public coracaoCheio = '/assets/coracao_cheio.png';
+  @Input()
+  public tentativas: number;
+
+  public coracoes: Coracao[] = [
+      new Coracao(true),
+      new Coracao(true),
+      new Coracao(true)
+  ];
 
   constructor() { }
 
